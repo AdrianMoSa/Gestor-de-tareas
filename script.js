3a7191498da0e9b27741adb2476b96df82fb933e
AdrianMoSa <adrianmoyasanchez@gmail.com>
2025-01-08 17:36:48 +0100

    proyecto finalizado

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const botonEnter = document.querySelector('#enter');
 const check = 'fa-check-circle';
 const uncheck = 'fa-circle';
 const lineTrough = 'line-through';
-let id = 0;
+let id;
+let LIST;
+
+
+//creación de fecha
+
+const FECHA = new Date()
+fecha.innerHTML = FECHA.toLocaleDateString('es',{weekday:'long',month:'short',day:'numeric'})
+
 
 
 //función agregar tarea.
@@ -31,14 +39,30 @@ function agregarTarea(tarea,id,realizado,eliminado){
 function tareaRealizada(element){
     element.classList.toggle(check);
     element.classList.toggle(uncheck);
-    element.parentNode.querySelector('.text').classList.toggle(lineTrough)
+    element.parentNode.querySelector('.text').classList.toggle(lineTrough);
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true;
 }
+
+//Función tarea eliminada
+
+function tareaEliminada(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].eliminado = true;
+}
+
 
 botonEnter.addEventListener('click',()=>{
     const tarea = input.value
     if(tarea){
         agregarTarea(tarea,id,false,false);
+        LIST.push({
+            nombre : tarea,
+            id: id,
+            realizado: false,
+            eliminado: false
+        })
     }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
     input.value='';
     id++;
 })
@@ -48,7 +72,14 @@ document.addEventListener('keyup',function(event){
         const tarea = input.value;
         if(tarea){
             agregarTarea(tarea,id,false,false);
+            LIST.push({
+                nombre: tarea,
+                id: id,
+                realizado: false,
+                eliminado: false
+            })
         }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
     input.value='';
     id++
     }
@@ -63,4 +94,24 @@ lista.addEventListener('click',function(event){
     else if (elementData === 'eliminado'){
         tareaEliminada(element);
     }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
 })
+
+
+//local storage get item
+
+let data = localStorage.getItem('TODO');
+if(data){
+    LIST = JSON.parse(data)
+    id = LIST.length
+    cargarLista(LIST)
+}else {
+    LIST = []
+    id = 0
+}
+
+function cargarLista(DATA){
+    DATA.forEach(function(i){
+        agregarTarea(i.nombre,i.id,i.realizado,i.eliminado);
+    })
+}
